678f6664353cbc28457833af215600d27415cf7f
Wavky.Huang <master@wavky.com>
2021-07-14 00:23:00 +0900

    Initial Commit

---
+++ SwiftUI-WeChatDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-WeChatDemo
+//
+//  Created by Wavky Huang on 2021/07/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-WeChatDemo/SwiftUI_WeChatDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeChatDemoApp.swift
+//  SwiftUI-WeChatDemo
+//
+//  Created by Wavky Huang on 2021/07/14.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeChatDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
